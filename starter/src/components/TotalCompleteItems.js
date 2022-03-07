dd0fca03cb08d1ef39bbfdee7569269c27a22c81
wasilahmad <wasilinfo@gmail.com>
2022-03-07 14:33:54 +0530

    displayed completed todo item count

--- starter/src/components/TotalCompleteItems.js
+++ starter/src/components/TotalCompleteItems.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const TotalCompleteItems = () => {
-	return <h4 className='mt-3'>Total Complete Items: 5</h4>;
+	const totalCompleteItems = useSelector((state)=> {
+		return state.todos.filter((todo) => todo.completed === true)
+	});
+	return <h4 className='mt-3'>Total Complete Items: {totalCompleteItems.length}</h4>;
 };
 
 export default TotalCompleteItems;
